88c4f11621a7b91d07dd3d807d9436bcca341ee4
Batfoxkid <batfoxkid@gmail.com>
2022-05-07 00:15:02 -0700

    Late Night Brain

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,11 +9,11 @@ mkdir -p package/addons/sourcemod/data
 mkdir -p package/addons/sourcemod/configs
 
 # Copy all required stuffs to package
-cp -r addons/sourcemod/translations/ff2_rewrite.phrases.txt package/addons/sourcemod/translations
+cp -r ../addons/sourcemod/translations/ff2_rewrite.phrases.txt package/addons/sourcemod/translations
 cp -r addons/sourcemod/plugins/freak_fortress_2.smx package/addons/sourcemod/plugins
 cp -r addons/sourcemod/plugins/ff2r_default_abilities.smx package/addons/sourcemod/plugins
 cp -r addons/sourcemod/plugins/ff2r_menu_abilities.smx package/addons/sourcemod/plugins
-cp -r addons/sourcemod/gamedata/ff2.txt package/addons/sourcemod/gamedata
+cp -r ../addons/sourcemod/gamedata/ff2.txt package/addons/sourcemod/gamedata
 cp -r ../addons/sourcemod/data/freak_fortress_2 package/addons/sourcemod/data
 cp -r ../addons/sourcemod/configs/freak_fortress_2 package/addons/sourcemod/configs
 cp -r ../models package
