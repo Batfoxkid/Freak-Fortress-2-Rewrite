546a736d6d103ad2f6b9fe7bde025933a6e92dfa
Batfoxkid <batfoxkid@gmail.com>
2022-08-30 08:31:14 -0700

    Action Fix 7

--- scripts/optional.sh
+++ scripts/optional.sh
@@ -1,6 +1,7 @@
 # Optional Includes
-wget "https://raw.githubusercontent.com/nosoop/SM-TFCustAttr/master/scripting/include/tf_custom_attributes.inc" -O include/tf_custom_attributes.inc
-wget "https://raw.githubusercontent.com/nosoop/SM-TFEconData/master/scripting/include/tf_econ_data.inc" -O include/tf_econ_data.inc
-wget "https://raw.githubusercontent.com/nosoop/SM-TFCustomWeaponsX/master/scripting/include/cwx.inc" -O include/cwx.inc
-wget "https://raw.githubusercontent.com/nosoop/SM-TFUtils/master/scripting/include/tf2utils.inc" -O include/tf2utils.inc
-wget "https://raw.githubusercontent.com/Flyflo/SM-Goomba-Stomp/master/addons/sourcemod/scripting/include/goomba.inc" -O include/goomba.inc
+cd $SCRIPTS_PATH
+wget "https://raw.githubusercontent.com/nosoop/SM-TFCustAttr/master/scripting/include/tf_custom_attributes.inc"
+wget "https://raw.githubusercontent.com/nosoop/SM-TFEconData/master/scripting/include/tf_econ_data.inc"
+wget "https://raw.githubusercontent.com/nosoop/SM-TFCustomWeaponsX/master/scripting/include/cwx.inc"
+wget "https://raw.githubusercontent.com/nosoop/SM-TFUtils/master/scripting/include/tf2utils.inc"
+wget "https://raw.githubusercontent.com/Flyflo/SM-Goomba-Stomp/master/addons/sourcemod/scripting/include/goomba.inc"
