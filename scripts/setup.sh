4e8d9e9c5b917f078c488e3d9eca88cb76cdacce
Batfoxkid <batfoxkid@gmail.com>
2022-08-30 08:47:25 -0700

    Don't even think about spamming meme commits

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,23 +1,21 @@
 # Environments
-echo "SCRIPTS_PATH=addons/sourcemod/scripting" >> $GITHUB_ENV
 echo "PLUGIN_VERSION_REVISION<<EOF" >> $GITHUB_ENV
 git rev-list --count HEAD >> $GITHUB_ENV
 echo 'EOF' >> $GITHUB_ENV
 
 # Set Version
-export PLUGIN_VERSION=$(sed -En '/#define PLUGIN_VERSION\W/p' addons/sourcemod/scripting/freak_fortress_2.sp)
+export PLUGIN_VERSION=$(sed -En '/#define PLUGIN_VERSION\W/p' freak_fortress_2.sp)
 echo "PLUGIN_VERSION<<EOF" >> $GITHUB_ENV
 echo $PLUGIN_VERSION | grep -o '[0-9]*\.[0-9]*' >> $GITHUB_ENV
 echo 'EOF' >> $GITHUB_ENV
 
-sed -i -e 's/#define PLUGIN_VERSION_REVISION.*".*"/#define PLUGIN_VERSION_REVISION "'$PLUGIN_VERSION_REVISION'"/g' addons/sourcemod/scripting/freak_fortress_2.sp
-for file in addons/sourcemod/scripting/ff2r_*.sp
+sed -i -e 's/#define PLUGIN_VERSION_REVISION.*".*"/#define PLUGIN_VERSION_REVISION "'$PLUGIN_VERSION_REVISION'"/g' freak_fortress_2.sp
+for file in $(find -type f -name "ff2r_*.sp")
 do
   sed -i -e 's/#define PLUGIN_VERSION.*".*"/#define PLUGIN_VERSION "'$PLUGIN_VERSION'.'$PLUGIN_VERSION_REVISION'"/g' $file
 done
 
 # Install Required Includes
-cd $SCRIPTS_PATH
 wget "https://raw.githubusercontent.com/DoctorMcKay/sourcemod-plugins/master/scripting/include/morecolors.inc"
 wget "https://raw.githubusercontent.com/peace-maker/DHooks2/dynhooks/sourcemod_files/scripting/include/dhooks.inc"
 wget "https://raw.githubusercontent.com/asherkin/TF2Items/master/pawn/tf2items.inc"
