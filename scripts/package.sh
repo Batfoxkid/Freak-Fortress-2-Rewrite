1d4e525b4283d95bed69737a221e965751656a57
Batfoxkid <batfoxkid@gmail.com>
2022-10-03 22:04:22 -0700

    Adjust

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,15 +1,18 @@
 # Epic Scout Package
 mkdir -p custom/epic_scout/addons/sourcemod/configs/freak_fortress_2
-mkdir -p custom/epic_scout/addons/sourcemod/configs/plugins
+mkdir -p custom/epic_scout/addons/sourcemod/plugins
 cp addons/sourcemod/configs/freak_fortress_2/non-default/epicscout.cfg custom/epic_scout/addons/sourcemod/configs/freak_fortress_2/epicscout.cfg
 cp addons/sourcemod/scripting/plugins/ff2r_epic_abilities.smx custom/epic_scout/addons/sourcemod/plugins/ff2r_epic_abilities.smx
 rm addons/sourcemod/scripting/plugins/ff2r_epic_abilities.smx
 
-# Main Package
-mkdir -p plugin
+# Default Package
 rmdir addons/sourcemod/configs/freak_fortress_2/non-default
-cp -r addons/sourcemod/scripting/plugins plugin
-cp -r addons/sourcemod/translations plugin
-cp -r addons/sourcemod/gamedata plugin
-cp -r addons/sourcemod/data plugin
-cp -r addons/sourcemod/configs plugin
+mkdir -p custom/ff2r_defaults/addons/sourcemod
+cp -r addons/sourcemod/configs custom/ff2r_defaults/addons/sourcemod/
+
+# Main Package
+mkdir -p plugin/addons/sourcemod
+cp -r addons/sourcemod/scripting/plugins plugin/addons/sourcemod
+cp -r addons/sourcemod/translations plugin/addons/sourcemod
+cp -r addons/sourcemod/gamedata plugin/addons/sourcemod
+cp -r addons/sourcemod/data plugin/addons/sourcemod
