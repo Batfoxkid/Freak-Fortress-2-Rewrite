c7f5adfce1dcfb09adc992ee68713ecfdfd9b579
Batfoxkid <batfoxkid@gmail.com>
2022-10-03 22:25:26 -0700

    Fixes

--- scripts/package.sh
+++ scripts/package.sh
@@ -2,11 +2,12 @@
 mkdir -p custom/epic_scout/addons/sourcemod/configs/freak_fortress_2
 mkdir -p custom/epic_scout/addons/sourcemod/plugins
 cp addons/sourcemod/configs/freak_fortress_2/non-default/epicscout.cfg custom/epic_scout/addons/sourcemod/configs/freak_fortress_2/epicscout.cfg
+rm addons/sourcemod/configs/freak_fortress_2/non-default/epicscout.cfg
 cp addons/sourcemod/scripting/plugins/ff2r_epic_abilities.smx custom/epic_scout/addons/sourcemod/plugins/ff2r_epic_abilities.smx
 rm addons/sourcemod/scripting/plugins/ff2r_epic_abilities.smx
 
 # Update Package
-mkdir -p plugin/addons/sourcemod
+mkdir -p update
 cp -r addons/sourcemod/scripting/plugins update
 cp -r addons/sourcemod/translations update
 cp -r addons/sourcemod/gamedata update
